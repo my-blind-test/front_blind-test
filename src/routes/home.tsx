eb4cce540000212dd5ac3f77ecf7c2f366e25e9e
RaphaelDk <raphael.dakiche@gmail.com>
2022-07-15 18:50:20 +0200

    Lobby gateway is now using JWT auth

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
 import HomeBar from "../Components/HomeBar";
@@ -11,29 +12,19 @@ export default function Home() {
     return (
         <div className="Home">
             <HomeBar />
-            <h1>Home</h1>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/lobby">Lobby</Link>
-                    </li>
-                    <li>
-                        <Link to="/game">Game</Link>
-                    </li>
-                    <li>
-                        <Link to="/user">User</Link>
-                    </li>
-                    <li>
-                        <Link to="/login">SignIn</Link>
-                    </li>
-                    <li>
-                        <Link to="/register">SignUp</Link>
-                    </li>
-                </ul>
-            </nav>
+            <h1>Home page</h1>
+            <Link to="/lobby" style={{ color: 'inherit', textDecoration: 'inherit' }}>
+                <Button color="inherit">
+                    Jouer
+                </Button>
+            </Link>
+            <p>Pour l'instant il n'y a pas grand chose mais à terme il y aura :</p>
+            <ul>
+                <li>Une fake partie qui tounne</li>
+                <li>Un super score board</li>
+                <li>Une explication des règles</li>
+                <li>Une explication de l'univers</li>
+            </ul>
         </div>
     );
 }
--- src/routes/lobby.tsx
+++ src/routes/lobby.tsx
@@ -1,7 +1,48 @@
+import { Button } from '@mui/material';
+import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+import { getStoredAccessToken } from '../utils/accessToken';
+
+const socket = io("http://localhost:3000/lobby", {
+    auth: {
+        token: "getStoredAccessTok"
+        // token: getStoredAccessToken()
+    }
+});
+
 export default function Lobby() {
+    // const [connectedUsers, setConnectedUsers] = useState([]);
+
+    useEffect(() => {
+        socket.on('connect', () => {
+            console.log("Connected")
+        });
+
+        socket.on('disconnect', () => {
+            console.log("Disconnected")
+        });
+
+        socket.on('users', (data) => {
+            console.log(data)
+        });
+
+        return () => {
+            socket.off('connect');
+            socket.off('disconnect');
+            socket.off('users');
+        };
+    }, []);
+
     return (
         <div className="Lobby">
             <h1>Lobby</h1>
+            <Button color="inherit"
+                onClick={() => {
+                    socket.emit("createGame", { name: "Hello", password: "password" })
+                }}>
+                Create Game
+            </Button>
+
         </div>
     );
 }

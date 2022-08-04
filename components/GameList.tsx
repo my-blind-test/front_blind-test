4e0fe3b051657ede43efc9e8321fa52b249cf798
RaphaelDk <raphael.dakiche@gmail.com>
2022-08-04 11:09:12 +0200

    Lobby : game relative logic moved into GameList component

--- components/GameList.tsx
+++ components/GameList.tsx
@@ -1,14 +1,14 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
-import { VariableSizeList, ListChildComponentProps } from 'react-window';
 import { useRouter } from 'next/router';
-import { Game } from '../utils/interfaces/Game';
 import { Avatar, Button, Container, Grid, List, ListItemAvatar, ListItemIcon } from '@mui/material';
 import LockIcon from '@mui/icons-material/Lock';
 import CreateGameModal from './CreateGameModal';
+import { useEffect, useRef, useState } from 'react';
+import { io } from 'socket.io-client';
+import { getStoredAccessToken } from '../utils/accessToken';
+import { Game } from '../utils/interfaces/Game';
 
 function GamePreview(props: { game: Game }) {
     const router = useRouter();
@@ -40,16 +40,77 @@ function GamePreview(props: { game: Game }) {
     )
 }
 
-export default function GameList(props: { data: Game[], onCreateGame: Function }) {
-    const [createGameModalOpen, setCreateGameModalOpen] = React.useState(false);
+export default function GameList() {
+    const [createGameModalOpen, setCreateGameModalOpen] = useState(false);
+    const [games, setGames] = useState<Game[]>([]);
+    const socket: any = useRef(null)
 
+    useEffect(() => {
+        socket.current = io("http://localhost:3000/lobby", {
+            auth: {
+                token: getStoredAccessToken()
+            }
+        });
+
+        return () => {
+            if (socket.current)
+                socket.current.disconnect();
+        };
+    }, [])
+
+    useEffect(() => {
+        if (socket.current) {
+            socket.current.removeAllListeners();
+
+            socket.current.on('connect', () => {
+                socket.current.emit('games', null, (response: any) => {
+                    setGames([...games, ...response.content])
+                    console.log(response.content)
+                })
+                console.log("Connected")
+            });
+
+            socket.current.on('disconnect', () => {
+                //Ramener à la page home
+                // Ca se passe si le jwt n'est pas bon à la connexion et peut etre dans un autre cas
+                // Donc peut etre check le JWT à l'entré du lobby
+            });
+
+            socket.current.on('newGame', (data: any) => {
+                setGames([...games, data])
+            });
+
+            socket.current.on('gameDeleted', (data: any) => {
+                setGames(games.filter(game => game.id !== data));
+            });
+
+            socket.current.on('gameUpdated', (data: any) => {
+                const newGames = [...games]
+                newGames[games.findIndex(game => game.id === data.id)] = data
+                setGames(newGames);
+            });
+
+            socket.current.on("connect_error", (err: any) => {
+                console.log("ERROR")
+                console.log(err)
+            });
+        }
+    }, [games])
+
+    const onCreateGame = async (name: string, password: string, playlistUrl: string) => {
+        const response = await new Promise(resolve =>
+            socket.current.emit('createGame', { name, password, playlistUrl }, (response: any) => resolve(response))
+        )
+
+        return response
+    }
     const onModalClose = (): void => {
         setCreateGameModalOpen(false)
     }
 
     return (
         <Container sx={{ mt: 15, backgroundColor: 'green' }}>
-            {createGameModalOpen && <CreateGameModal open={createGameModalOpen} onModalClose={onModalClose} onCreateGame={props.onCreateGame} />}
+            {createGameModalOpen && <CreateGameModal open={createGameModalOpen} onModalClose={onModalClose} onCreateGame={onCreateGame} />}
             <Grid container rowSpacing={15} direction="column" sx={{}}>
                 <Grid item container spacing={2} rowSpacing={8} > {/*TODO : align center*/}
                     <Grid item xs={6}>
@@ -62,7 +123,7 @@ export default function GameList(props: { data: Game[], onCreateGame: Function }
                 <Grid item xs={12}>
                     <List sx={{ overflow: 'auto', maxHeight: '600px', bgcolor: 'red' }}>
                         {
-                            props.data.map((game: any, key: any) =>
+                            games.map((game: any, key: any) =>
                                 <GamePreview game={game} key={key} />
                             )
                         }
--- pages/lobby.tsx
+++ pages/lobby.tsx
@@ -1,94 +1,13 @@
 import { Box, Container, Grid } from '@mui/material';
-import Image from 'next/image';
-import { useEffect, useRef, useState } from 'react';
-import { io } from 'socket.io-client';
 import GameList from '../components/GameList';
 import UserPreview from '../components/UserPreview';
-import { getStoredAccessToken } from '../utils/accessToken';
-import { ConnectedUser } from '../utils/interfaces/ConnectedUser';
-import { Game } from '../utils/interfaces/Game';
 
 export default function Lobby() {
-    const [connectedUsers, setConnectedUsers] = useState<ConnectedUser[]>([]);
-    const [games, setGames] = useState<Game[]>([]);
-    const socket: any = useRef(null)
-
-    useEffect(() => {
-        socket.current = io("http://localhost:3000/lobby", {
-            auth: {
-                token: getStoredAccessToken()
-            }
-        });
-
-        return () => {
-            if (socket.current)
-                socket.current.disconnect();
-        };
-    }, [])
-
-    useEffect(() => {
-        if (socket.current) {
-            socket.current.removeAllListeners();
-
-            socket.current.on('connect', () => {
-                socket.current.emit('users', null, (response: any) => {
-                    setConnectedUsers([...connectedUsers, ...response.content])
-                })
-                socket.current.emit('games', null, (response: any) => {
-                    setGames([...games, ...response.content])
-                    console.log(response.content)
-                })
-                console.log("Connected")
-            });
-
-            socket.current.on('disconnect', () => {
-                //Ramener à la page home
-                // Ca se passe si le jwt n'est pas bon à la connexion et peut etre dans un autre cas
-                // Donc peut etre check le JWT à l'entré du lobby
-            });
-
-            socket.current.on('userJoined', (data: ConnectedUser) => {
-                setConnectedUsers([...connectedUsers, data])
-            });
-
-            socket.current.on('userLeft', (data: any) => {
-                setConnectedUsers(connectedUsers.filter(connectedUser => connectedUser.clientId !== data));
-            });
-
-            socket.current.on('newGame', (data: any) => {
-                setGames([...games, data])
-            });
-
-            socket.current.on('gameDeleted', (data: any) => {
-                setGames(games.filter(game => game.id !== data));
-            });
-
-            socket.current.on('gameUpdated', (data: any) => {
-                const newGames = [...games]
-                newGames[games.findIndex(game => game.id === data.id)] = data
-                setGames(newGames);
-            });
-
-            socket.current.on("connect_error", (err: any) => {
-                console.log("ERROR")
-                console.log(err)
-            });
-        }
-    }, [games, connectedUsers])
-
-    const onCreateGame = async (name: string, password: string, playlistUrl: string) => {
-        const response = await new Promise(resolve =>
-            socket.current.emit('createGame', { name, password, playlistUrl }, (response: any) => resolve(response))
-        )
-
-        return response
-    }
-
     return (
         <Container maxWidth={false} sx={{ flexGrow: 1 }}>
             <Grid container spacing={2} direction="row" alignItems="flex-start">
                 <Grid item xs={12} md={6} lg={4}>
-                    <GameList data={games} onCreateGame={onCreateGame} />
+                    <GameList />
                 </Grid>
                 <Grid item xs={12} md={6} lg={4}>
                     <UserPreview />
